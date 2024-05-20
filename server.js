fc8dd6cbfc4d4848fa7aa5791a67c1cccfb02b04
Peter Njoroge <peterkimani2005@yahoo.com>
2024-05-20 16:33:34 -0400

    Changed the logic in the price settings

--- server.js
+++ server.js
@@ -1,41 +1,33 @@
-const express = require('express');
-const app = express();
+// netlify/functions/product.js
 const SneaksAPI = require('sneaks-api');
 const sneaks = new SneaksAPI();
 
-app.use('/Images', express.static('Images'));
+exports.handler = async (event, context) => {
+  const productId = event.path.split('/').pop();
 
-app.get('/product/:productId', (req, res) => {
-  app.use('/Images', express.static('Images'));
-  const productId = req.params.productId;
-  sneaks.getProductPrices(productId, function(err, product){
-    if (err) {
-      console.error('Error getting product prices:', err);
-      res.send(`Error getting product prices: ${err.message}`);
-    } else {
-      // Get the price values
-      const stockXPrice = product.lowestResellPrice.stockX;
-      const goatPrice = product.lowestResellPrice.goat;
-      const flightClubPrice = product.lowestResellPrice.flightClub;
+  return new Promise((resolve, reject) => {
+    sneaks.getProductPrices(productId, function(err, product){
+      if (err) {
+        console.error('Error getting product prices:', err);
+        resolve({
+          statusCode: 500,
+          body: `Error getting product prices: ${err.message}`
+        });
+      } else {
+        // Get the price values
+        const stockXPrice = product.lowestResellPrice.stockX;
+        const goatPrice = product.lowestResellPrice.goat;
+        const flightClubPrice = product.lowestResellPrice.flightClub;
 
-      res.send(`
-        <html>
-          <head>
-            <title>Product Prices</title>
-          </head>
-          <body>
-            <h1>Product Prices</h1>
-            <img src="Images/sneakers/jordan1chicago.jpg">
-            <p>StockX Price: ${stockXPrice}+</p>
-            <p>Goat Price: ${goatPrice}+</p>
-            <p>Flight Club Price: ${flightClubPrice}+</p>
-          </body>
-        </html>
-      `);
-    }
+        resolve({
+          statusCode: 200,
+          body: JSON.stringify({
+            stockXPrice: stockXPrice,
+            goatPrice: goatPrice,
+            flightClubPrice: flightClubPrice
+          })
+        });
+      }
+    });
   });
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+};
